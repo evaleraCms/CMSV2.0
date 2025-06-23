e0d42c76ac750737ddb342ea2f092fe245a6a333
Enmanuel Valera <valera.enmanuel@hotmail.com>
2025-06-22 20:44:09 -0400

    CMS20 Clean Architectrure part 1

---
+++ CMS.RestApi/CMS.Test/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿namespace CMS.Test
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
